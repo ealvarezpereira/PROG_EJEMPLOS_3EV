921b7e691a3ef21d0ee922107d6c393ddc25856b
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-04-12 14:18:19 +0200

    Ejemplo 1 de la paleta de programacion

---
+++ GUI1_EJ/src/gui1_ej/GUI1_EJ.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui1_ej;
+
+/**
+ *
+ * @author quique
+ */
+public class GUI1_EJ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana vent = new Ventana();
+        vent.setVisible(true);
+    }
+    
+}
